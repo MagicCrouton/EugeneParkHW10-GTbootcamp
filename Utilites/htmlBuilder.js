971982c59bbf21e06bac68443aa44b6a5edd7a51
Eugene Park <e.park5336@gmail.com>
2023-01-15 02:01:08 -0500

    added readme file generated from previous HW

--- Utilites/htmlBuilder.js
+++ Utilites/htmlBuilder.js
@@ -7,21 +7,20 @@ const htmlArrangment = [];
 function buildSite(team){
     team.forEach((element, index) => {
     if (element.getRole() === 'Manager') {
-       let uniqueOut = element.officeNumber;
+       var uniqueOut = `My office number is ${element.officeNumber}`;
     }
     else if (element.getRole()==='Engineer') {
-        let uniqueOut = element.github;
+        var uniqueOut = `Here's my gitHub ID ${element.github}`;
     }
     else {
-        let uniqueOut = element.school;
+        var uniqueOut = `I am Studying at${element.school}`;
     }
     htmlArrangment[index] =
         `<div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title">${element.name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${element.getRole()}</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">${element.email}</a>
+            <a href="mailto:${element.email}" class="card-link">${element.email}</a>
             <a href="#" class="card-link">${uniqueOut}</a>
         </div>
         </div>`
@@ -36,12 +35,19 @@ function buildSite(team){
         <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
         <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
       </head>
-      <body>`)
+      <body>
+      <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Team Managed by ${team[0].name}</h1>
+        <p class="lead">How much work can you squeeze out of these poor saps before they quit??</p>
+      </div>
+        </div>
+      <div class="d-flex flex-row">`)
     htmlArrangment.push(
-        `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        `</div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-        <script src="./index.js"></script>
     </body>
     </html>`)
     let payload = htmlArrangment.join('');
