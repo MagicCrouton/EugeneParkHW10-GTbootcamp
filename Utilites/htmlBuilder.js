6365aebfe7aaf34748b1e35e75e8c12a602edce5
Eugene Park <e.park5336@gmail.com>
2023-01-15 01:22:54 -0500

    the html builder is now working properly as far as node and fs goes

--- Utilites/htmlBuilder.js
+++ Utilites/htmlBuilder.js
@@ -1,28 +1,11 @@
 const fs = require('fs');
-const baseHtml = `<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Team O Matic 5000</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-  </head>
-  <body>
-    
+const htmlArrangment = [];
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-    <script src="./index.js"></script>
-  </body>
-</html>`
+// note: i would use express for this portion but i wanted to see how to build a site without express
 
-async function buildSite(team){
-    await fs.writeFile(`../dist/managedby${team[0].Name}.html`, baseHtml)
-    team.forEach(element => {
-        $('body').append(
+function buildSite(team){
+    team.forEach((element, index) => {
+    htmlArrangment[index] =
         `<div class="card" style="width: 18rem;">
         <div class="card-body">
             <h5 class="card-title">${element.Name}</h5>
@@ -31,11 +14,39 @@ async function buildSite(team){
             <a href="#" class="card-link">${element.email}</a>
             <a href="#" class="card-link">Another link</a>
         </div>
-        </div>`)
+        </div>`
     });
-}
+    htmlArrangment.unshift(`<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Team O Matic 5000</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+      </head>
+      <body>`)
+    htmlArrangment.push(
+        `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+        <script src="./index.js"></script>
+    </body>
+    </html>`)
+    let payload = htmlArrangment.join('');
+    console.log(payload);
+    fs.writeFile(`./dist/teamManagedby${team[0].name}.html`, payload, err => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log(`file teamManagedby${team[0].name}}.html written successfully`)
+        }
+    });
 
+}
+// C:\Users\eugen\gt-bootcamp\homework\EugeneParkHW10-GTbootcamp\dist
 module.exports = {
-    baseHtml,
     buildSite
 }
